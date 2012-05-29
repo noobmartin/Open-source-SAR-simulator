57d93c6d963d9750c51d3b65300c186ca6b31d73
David Sveningsson <ext@sidvind.com>
2012-05-29 20:35:06 +0200

    Adding proper Makefile.

--- compile.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gcc waveforms.c algorithms.c sar_simulator.c file_io.c filters.c -lfftw3 -lm -O3 -fomit-frame-pointer -fstrict-aliasing -ffast-math -msse2 -march=native -mcmodel=medium -g -o sar_simulator
